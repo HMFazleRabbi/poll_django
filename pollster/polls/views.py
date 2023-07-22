47465bdacb54193557d81aac462290ddd0b46f99
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-07-23 01:13:50 +0600

    Setting up view.

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+# Models
+from .models import Question, Choice
+
 # Create your views here.
+# Get question and display them
+def index(request):
+    return render(request, 'polls/index.html')
