14aac39dfbe24a2763d512f5abf00f80e23df0eb
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-07-23 01:40:14 +0600

    Added content

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -6,4 +6,6 @@ from .models import Question, Choice
 # Create your views here.
 # Get question and display them
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context=context)
