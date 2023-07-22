e4c2fad7a4858feecf6fa724c981952675b0d6c8
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-07-23 01:13:33 +0600

    Added header to admin page

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+admin.site.site_header="Pollester Admin"
+admin.site.site_title="Pollester Admin Area"
+admin.site.index_title="Welcome to Pollester Admin Area."
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
